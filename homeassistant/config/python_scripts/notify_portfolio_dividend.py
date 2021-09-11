2614c3954d62aec839540db2ce3bd5970a2c7412
Claes Hallström <hallstrom.claes@gmail.com>
2021-09-11 21:39:51 +0200

    Add python script to send portfolio dividends

---
+++ homeassistant/config/python_scripts/notify_portfolio_dividend.py
@@ -0,0 +1,90 @@
+"""Send a notification with portfolio dividend to telegram."""
+hass = hass  # noqa: F821
+data = data  # noqa: F821
+logger = logger  # noqa: F821
+MESSAGE_MAX_WIDTH = 41
+
+
+def get_entity(entity_id):
+    """Get entity."""
+    return hass.states.get(entity_id)
+
+
+def convert_to_sek(amount, currency):
+    """Convert CAD, EUR or USD to SEK."""
+    convert = {
+        "EUR": float(get_entity("sensor.currency_eur_sek").state),
+        "USD": float(get_entity("sensor.currency_usd_sek").state),
+        "SEK": 1.0,
+    }
+    if currency not in convert.keys():
+        logger.error("Unknown currency: %s" % currency)
+        return 0
+    return amount * convert[currency]
+
+
+def get_value(entity, value_key):
+    """Get value of entity in correct currency."""
+    return convert_to_sek(
+        entity.attributes[value_key], entity.attributes["unit_of_measurement"]
+    )
+
+
+def to_string(data):
+    """Convert óbject to string, potentially format."""
+    if isinstance(data, float):
+        return "{:.2f}".format(data)
+    if isinstance(data, int):
+        return "{}".format(data)
+    return data
+
+
+def adjust_lenght(string, max_length):
+    """Adjust the length of string."""
+    while len(string) > max_length:
+        string = string[:-1]
+    return string
+
+
+stocks = []
+for entity_id in hass.states.entity_ids("sensor"):
+    if entity_id.startswith("sensor.stock_"):
+        stocks.append(entity_id)
+
+# Get message data
+dividends = []
+for entity_id in stocks:
+    entity = get_entity(entity_id)
+
+    for payment_date in [
+        "dividend0_paymentDate",
+        "dividend1_paymentDate",
+        "dividend2_paymentDate",
+    ]:
+        if payment_date not in entity.attributes:
+            break
+        date = entity.attributes[payment_date]
+        if date == "unknown":
+            break
+        name = entity.attributes["name"]
+        amount_per_share = get_value(
+            entity, payment_date.split("_")[0] + "_amountPerShare"
+        )
+        shares = entity.attributes["shares"]
+        dividends.append((date, name, to_string(amount_per_share * shares)))
+
+dividends.sort()
+
+# Create message
+message = "<b>Stock Portfolio Dividends</b><code>\n"
+message = message + "-" * MESSAGE_MAX_WIDTH + "\n"
+for (date, name, amount) in dividends:
+    date = date.ljust(len(date) + 1)
+    amount = amount.rjust(len("10000.00") + 1)
+    name = adjust_lenght(name, MESSAGE_MAX_WIDTH - len(date) - len(amount))
+    space = " " * (MESSAGE_MAX_WIDTH - len(date) - len(name) - len(amount))
+    message = message + "{}{}{}{}\n".format(date, name, space, amount)
+message = message + "</code>"
+
+# Send notification
+hass.services.call("notify", "telegram", {"message": message})
