d8d83ddfc14f9cbcfa2454b7f8c5cb21f1f47100
Claes Hallström <hallstrom.claes@gmail.com>
2023-07-02 14:20:07 +0200

    Update oci terraform to create two instances

--- terraform/oracle-cloud-infrastructure/compute.tf
+++ terraform/oracle-cloud-infrastructure/compute.tf
@@ -4,6 +4,7 @@ data "oci_identity_availability_domain" "availability_domain" {
 }
 
 resource "oci_core_instance" "instance" {
+  count               = 2
   availability_domain = data.oci_identity_availability_domain.availability_domain.name
   compartment_id      = oci_identity_compartment.compartment.id
   shape               = "VM.Standard.E2.1.Micro"
@@ -11,7 +12,7 @@ resource "oci_core_instance" "instance" {
     source_id   = "ocid1.image.oc1.eu-stockholm-1.aaaaaaaaueqwi7bpc5teyemjxum2eqsy566w4cam3jjsdcgakbwi6zanzwia"
     source_type = "image"
   }
-  display_name = "cloud-monitor"
+  display_name = "oci-${count.index}"
   create_vnic_details {
     assign_public_ip = true
     subnet_id        = oci_core_subnet.subnet.id
@@ -23,5 +24,5 @@ resource "oci_core_instance" "instance" {
 }
 
 output "public-ip-for-instance" {
-  value = oci_core_instance.instance.public_ip
+  value = [for i in oci_core_instance.instance : i.public_ip]
 }
