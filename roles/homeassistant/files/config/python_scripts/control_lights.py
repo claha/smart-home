85835bd99dd83c7fd842622725813b4aca0f121f
Claes Hallström <hallstrom.claes@gmail.com>
2023-11-16 12:49:40 +0100

    Use python script to control lights

---
+++ roles/homeassistant/files/config/python_scripts/control_lights.py
@@ -0,0 +1,140 @@
+"""Control lights."""
+hass = hass  # noqa: F821
+data = data  # noqa: F821
+logger = logger  # noqa: F821
+
+ACTION_ON = "on"
+ACTION_DIM = "dim"
+ACTION_OFF = "off"
+
+
+def light_on(entity_id):
+    """Turn on a light."""
+    hass.services.call(
+        "light",
+        "turn_on",
+        {
+            "entity_id": entity_id,
+            "brightness_pct": 80,
+            "color_temp": 454,
+            "transition": 10,
+        },
+        False,
+    )
+
+
+def light_dim(entity_id):
+    """Dim a light."""
+    hass.services.call(
+        "light",
+        "turn_on",
+        {
+            "entity_id": entity_id,
+            "brightness_pct": 10,
+            "color_temp": 454,
+            "transition": 10,
+        },
+        False,
+    )
+
+
+def light_off(entity_id):
+    """Turn off a light."""
+    hass.services.call(
+        "light",
+        "turn_off",
+        {
+            "entity_id": entity_id,
+            "transition": 10,
+        },
+        False,
+    )
+
+
+def switch_on(entity_id):
+    """Turn on a switch."""
+    hass.services.call(
+        "switch",
+        "turn_on",
+        {
+            "entity_id": entity_id,
+        },
+        False,
+    )
+
+
+def switch_dim(entity_id):
+    """Dim a switch."""
+    pass
+
+
+def switch_off(entity_id):
+    """Turn off a switch."""
+    hass.services.call(
+        "switch",
+        "turn_off",
+        {
+            "entity_id": entity_id,
+        },
+        False,
+    )
+
+
+on_control = {
+    "light": light_on,
+    "switch": switch_on,
+}
+
+dim_control = {
+    "light": light_dim,
+    "switch": switch_dim,
+}
+
+off_control = {
+    "light": light_off,
+    "switch": switch_off,
+}
+
+
+lights = {
+    "master_bedroom": [
+        "light.tradfri_bulb_0",
+    ],
+    "guest_room": [
+        "switch.shelly_plug_s_0",
+    ],
+    "kitchen": [
+        "switch.tradfri_control_outlet_1",
+        # "switch.osram_smart_plug_1",  # Seems broken?
+    ],
+    "living_room": [
+        "switch.osram_smart_plug_0",
+        "switch.tradfri_control_outlet_0",
+        "switch.shelly_plug_s_1",
+    ],
+    "laundry": [
+        "light.tradfri_bulb_2",
+    ],
+    "closet": [
+        "light.bulb_rgbw_8b5534",
+    ],
+    "kids_bedroom": [
+        "switch.tradfri_control_outlet_2",
+        "switch.tradfri_control_outlet_3",
+    ],
+}
+
+# Get script arguments
+area = data.get("area")
+action = data.get("action")
+
+# Perform action on each entity id
+entity_ids = lights[area]
+for entity_id in entity_ids:
+    entity_type = entity_id.split(".")[0]
+    if action == ACTION_ON:
+        on_control[entity_type](entity_id)
+    elif action == ACTION_DIM:
+        dim_control[entity_type](entity_id)
+    elif action == ACTION_OFF:
+        off_control[entity_type](entity_id)
