04972d788e11fad1c3376f2c8a284e1344ebd70b
Claes Hallström <hallstrom.claes@gmail.com>
2024-02-25 21:53:20 +0100

    Run jellyfin as a service instead of docker container

--- roles/nixos/files/services/jellyfin.nix
+++ roles/nixos/files/services/jellyfin.nix
@@ -2,31 +2,28 @@
 
 let
   secrets = import ../secrets.nix { inherit config pkgs; };
-  jellyfinPort = 8096;
 in
 {
-  virtualisation.oci-containers.containers.jellyfin = {
-    autoStart = true;
-    image = "docker.io/jellyfin/jellyfin:10.8.13";
-    volumes = [
-      "/etc/jellyfin/config:/config"
-      "/etc/jellyfin/cache:/cache"
-      "/etc/jellyfin/log:/log"
-      "/media:/media"
-    ];
-    ports = [ "${toString jellyfinPort}:${toString jellyfinPort}" ];
-    environment = {
-      JELLYFIN_LOG_DIR = "/log";
-      JELLYFIN_PublishedServerUrl = "jellyfin.media.${secrets.domain}";
-    };
+  services.jellyfin = {
+    enable = true;
+    package = pkgs.unstable.jellyfin;
+    user = "jellyfin";
+    group = "jellyfin";
+    openFirewall = true;
   };
 
   services.nginx.virtualHosts."jellyfin.media.${secrets.domain}" = {
     useACMEHost = "${secrets.domain}";
     acmeRoot = null;
     forceSSL = true;
-    locations."/" = { proxyPass = "http://127.0.0.1:${toString jellyfinPort}"; proxyWebsockets = true; };
+    locations."/" = { proxyPass = "http://127.0.0.1:8096"; proxyWebsockets = true; };
   };
 
-  networking.firewall.allowedTCPPorts = [ jellyfinPort ];
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      intel-compute-runtime
+    ];
+  };
 }
