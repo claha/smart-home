2c9612da3499d40748ed2be79273f59f171cd735
Claes Hallström <hallstrom.claes@gmail.com>
2024-08-10 21:48:37 +0200

    nixos: allow luffy to read duckdns token

--- nixos/secrets/secrets.nix
+++ nixos/secrets/secrets.nix
@@ -1,7 +1,16 @@
 let
-  chewbacca = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKrVgUdJlyiqFnzWQdp1wqdfInM2f2zObzQR/NY6apoo";
-  claes = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGO2k5rXeGp/j8osRdxDfsv6GJ3ngmMic2yalepytUzb";
+  chewbacca = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBwSq5lZGTwwD48EwSs+/go931lbRpi5yVuRJQR6rD3d"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKrVgUdJlyiqFnzWQdp1wqdfInM2f2zObzQR/NY6apoo"
+  ];
+  claes = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGO2k5rXeGp/j8osRdxDfsv6GJ3ngmMic2yalepytUzb"
+  ];
+  luffy = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMlhYm7+ZyPcFTCjqpgz3QPgedfSEq1M6dkNKY0way69"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEWCgyl+ULikm6SQdNFAHB80ffrpcu4fwVk7l+epZckl"
+  ];
 in
 {
-  "duckdns-token.age".publicKeys = [ chewbacca claes ];
+  "duckdns-token.age".publicKeys = chewbacca ++ claes ++ luffy;
 }
