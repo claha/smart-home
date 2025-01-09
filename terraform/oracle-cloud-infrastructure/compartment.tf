a38633c7c76e9081b464bdb1e9bdbae19dbb6fdf
Claes Hallström <hallstrom.claes@gmail.com>
2025-01-09 14:34:35 +0100

    chore: cleanup unsued things

--- terraform/oracle-cloud-infrastructure/compartment.tf
+++
@@ -1,5 +0,0 @@
-resource "oci_identity_compartment" "compartment" {
-  compartment_id = var.tenancy_ocid
-  description    = "Compartment for Terraform"
-  name           = "terraform-compartment"
-}
