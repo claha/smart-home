7ae969d312467b65632d934614ae9d82aeb661e8
Claes Hallström <hallstrom.claes@gmail.com>
2025-04-24 22:45:54 +0200

    nixos: fix nltk data for mealie

--- nixos/services/mealie.nix
+++ nixos/services/mealie.nix
@@ -3,6 +3,9 @@
   services.mealie = {
     enable = true;
     package = pkgs.unstable.mealie;
+    settings = {
+      NLTK_DATA = "/var/lib/nltk_data";
+    };
   };
 
   networking.firewall.allowedTCPPorts = [ 9000 ];
