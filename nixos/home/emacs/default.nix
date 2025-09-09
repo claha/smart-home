c8dafa090129c10bc134a202f460b966878a9dc2
Claes Hallström <hallstrom.claes@gmail.com>
2025-09-09 21:51:41 +0200

    nixos: more emacs config

--- nixos/home/emacs/default.nix
+++ nixos/home/emacs/default.nix
@@ -2,5 +2,40 @@
 {
   programs.emacs = {
     enable = true;
+    extraPackages = epkgs: [
+      epkgs.ef-themes
+      epkgs.prescient
+      epkgs.vertico
+      epkgs.vertico-prescient
+      epkgs.nix-ts-mode
+      (epkgs.treesit-grammars.with-grammars (ts: [
+        ts.tree-sitter-nix
+      ]))
+    ];
+    extraConfig = ''
+      (use-package ef-themes
+        :init
+        (load-theme 'ef-maris-dark t))
+
+      (use-package savehist
+        :init
+        (savehist-mode))
+
+      (use-package prescient
+        :config
+        (prescient-persist-mode))
+
+      (use-package vertico
+        :init
+        (vertico-mode))
+
+      (use-package vertico-prescient
+        :after vertico prescient
+        :init
+        (vertico-prescient-mode))
+
+      (use-package nix-ts-mode
+        :mode "\\.nix\\'")
+    '';
   };
 }
