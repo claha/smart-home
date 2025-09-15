25fcfbac5328768a71bbd1806c8d87eda3fbfa32
Claes Hallström <hallstrom.claes@gmail.com>
2025-09-15 15:04:51 +0200

    feat(services): update ip for eren

--- nixos/services/karakeep.nix
+++ nixos/services/karakeep.nix
@@ -6,7 +6,7 @@
     extraEnvironment = {
       CRAWLER_VIDEO_DOWNLOAD = "true";
       CRAWLER_VIDEO_DOWNLOAD_MAX_SIZE = "-1";
-      OLLAMA_BASE_URL = "http://192.168.1.160:11434";
+      OLLAMA_BASE_URL = "http://192.168.1.228:11434";
       INFERENCE_TEXT_MODEL = "llama3.2:1b";
       INFERENCE_IMAGE_MODEL = "llava-phi3";
       INFERENCE_CONTEXT_LENGTH = "4096";
