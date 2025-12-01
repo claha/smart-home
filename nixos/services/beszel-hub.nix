d7ba1a8efc0d18f74cae4917bbd72eef8462f794
Claes Hallström <hallstrom.claes@gmail.com>
2025-12-01 13:45:10 +0100

    feat(beszel-hub): use default port

--- nixos/services/beszel-hub.nix
+++ nixos/services/beszel-hub.nix
@@ -11,23 +11,17 @@ in
 {
   options.homelab.beszel-hub = {
     enable = lib.mkEnableOption "Beszel Hub";
-    port = lib.mkOption {
-      type = lib.types.port;
-      default = 3001;
-      description = "Port for Beszel Hub";
-    };
   };
 
   config = lib.mkIf cfg.enable {
     services.beszel.hub = {
       enable = true;
       host = "0.0.0.0";
-      port = cfg.port;
       environment = {
         SHARE_ALL_SYSTEMS = "true";
       };
     };
 
-    networking.firewall.allowedTCPPorts = [ cfg.port ];
+    networking.firewall.allowedTCPPorts = [ config.services.beszel.hub.port ];
   };
 }
