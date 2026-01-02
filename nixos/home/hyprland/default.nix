811643156c88364060ed076f6e800fe0edba43f8
Claes Hallström <hallstrom.claes@gmail.com>
2026-01-02 17:48:31 +0100

    feat(hyprland): fuzzel power menu

--- nixos/home/hyprland/default.nix
+++ nixos/home/hyprland/default.nix
@@ -25,123 +25,129 @@ in
 
   wayland.windowManager.hyprland = {
     enable = true;
-    settings = {
-      "$mod" = "SUPER";
-      "$terminal" = "wezterm";
-      "$menu" = "fuzzel";
-      "$browser" = "firefox";
-      exec-once = [
-        "waybar"
-        "hyprpaper"
-        "hypridle"
-        "wl-paste --watch cliphist store"
-        "hyprlock"
-      ];
-      monitor = ",preferred,auto,1.0";
-      workspace = [
-        "1,monitor:eDP-1"
-        "2,monitor:eDP-1"
-        "3,monitor:eDP-1"
-        "4,monitor:eDP-1"
-        "5,monitor:eDP-1"
-        "6,monitor:DP-1"
-        "7,monitor:DP-1"
-        "8,monitor:DP-1"
-        "9,monitor:DP-1"
-      ];
-      windowrulev2 = "noborder, onworkspace:w[t1]";
-      bind = [
-        "$mod, F, fullscreen"
-        "$mod, RETURN, exec, $terminal"
-        "$mod SHIFT, Q, killactive"
-        "$mod, D, exec, $menu"
-        "$mod, B, exec, $browser"
-        "$mod, M, exit"
-        "$mod, P, pseudo"
-        "$mod, J, togglesplit"
-        "$mod, left, movefocus, l"
-        "$mod, right, movefocus, r"
-        "$mod, up, movefocus, u"
-        "$mod, down, movefocus, d"
-        "$mod, L, exec, hyprlock"
-        "$mod, V, exec, cliphist list | fuzzel --dmenu --width 100 | cliphist decode | wl-copy"
-      ]
-      ++ (
-        # workspaces
-        # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
-        builtins.concatLists (
-          builtins.genList (
-            i:
-            let
-              ws = i + 1;
-            in
-            [
-              "$mod, code:1${toString i}, workspace, ${toString ws}"
-              "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-            ]
-          ) 9
-        )
-      );
+    settings =
+      let
+        powerMenuCommand = "printf 'Lock\\nLogout\\nReboot\\nShutdown' | fuzzel --dmenu --width 50 --minimal-lines | xargs -I {} sh -c 'case \"$1\" in Lock) hyprlock ;; Logout) hyprctl dispatch exit ;; Reboot) systemctl reboot ;; Shutdown) systemctl poweroff ;; esac' sh {}";
+      in
+      {
+        "$mod" = "SUPER";
+        "$terminal" = "wezterm";
+        "$menu" = "fuzzel";
+        "$browser" = "firefox";
+        "$power_menu" = powerMenuCommand;
+        exec-once = [
+          "waybar"
+          "hyprpaper"
+          "hypridle"
+          "wl-paste --watch cliphist store"
+          "hyprlock"
+        ];
+        monitor = ",preferred,auto,1.0";
+        workspace = [
+          "1,monitor:eDP-1"
+          "2,monitor:eDP-1"
+          "3,monitor:eDP-1"
+          "4,monitor:eDP-1"
+          "5,monitor:eDP-1"
+          "6,monitor:DP-1"
+          "7,monitor:DP-1"
+          "8,monitor:DP-1"
+          "9,monitor:DP-1"
+        ];
+        windowrulev2 = "noborder, onworkspace:w[t1]";
+        bind = [
+          "$mod, F, fullscreen"
+          "$mod, RETURN, exec, $terminal"
+          "$mod SHIFT, Q, killactive"
+          "$mod, D, exec, $menu"
+          "$mod, B, exec, $browser"
+          "$mod, M, exit"
+          "$mod, P, pseudo"
+          "$mod, J, togglesplit"
+          "$mod, left, movefocus, l"
+          "$mod, right, movefocus, r"
+          "$mod, up, movefocus, u"
+          "$mod, down, movefocus, d"
+          "$mod, L, exec, hyprlock"
+          "$mod, V, exec, cliphist list | fuzzel --dmenu --width 100 | cliphist decode | wl-copy"
+          "$mod SHIFT, P, exec, $power_menu"
+        ]
+        ++ (
+          # workspaces
+          # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+          builtins.concatLists (
+            builtins.genList (
+              i:
+              let
+                ws = i + 1;
+              in
+              [
+                "$mod, code:1${toString i}, workspace, ${toString ws}"
+                "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+              ]
+            ) 9
+          )
+        );
 
-      bindm = [
-        "$mod, ALT_L, resizewindow"
-      ];
+        bindm = [
+          "$mod, ALT_L, resizewindow"
+        ];
 
-      binde = [
-        "$mod ALT, right, resizeactive, 5 0"
-        "$mod ALT, left, resizeactive, -5 0"
-        "$mod ALT, up, resizeactive, 0 -5"
-        "$mod ALT, down, resizeactive, 0 5"
-      ];
+        binde = [
+          "$mod ALT, right, resizeactive, 5 0"
+          "$mod ALT, left, resizeactive, -5 0"
+          "$mod ALT, up, resizeactive, 0 -5"
+          "$mod ALT, down, resizeactive, 0 5"
+        ];
 
-      bindel = [
-        ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 1%+"
-        ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%-"
-        ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-        ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-        ",XF86MonBrightnessUp, exec, brightnessctl -e4 -n2 set 1%+"
-        ",XF86MonBrightnessDown, exec, brightnessctl -e4 -n2 set 1%-"
-      ];
-      input = {
-        kb_layout = "se";
-        kb_options = "caps:none";
-        follow_mouse = 0;
-        sensitivity = 0;
-        touchpad = {
-          natural_scroll = false;
+        bindel = [
+          ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 1%+"
+          ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 1%-"
+          ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+          ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+          ",XF86MonBrightnessUp, exec, brightnessctl -e4 -n2 set 1%+"
+          ",XF86MonBrightnessDown, exec, brightnessctl -e4 -n2 set 1%-"
+        ];
+        input = {
+          kb_layout = "se";
+          kb_options = "caps:none";
+          follow_mouse = 0;
+          sensitivity = 0;
+          touchpad = {
+            natural_scroll = false;
+          };
+        };
+
+        general = {
+          gaps_in = 5;
+          gaps_out = 10;
+          border_size = 2;
+          "col.active_border" = "rgb(e66100)";
+          "col.inactive_border" = "rgba(e6610044)";
+          resize_on_border = false;
+          allow_tearing = false;
+          layout = "dwindle";
+        };
+
+        dwindle = {
+          pseudotile = true;
+          preserve_split = true;
+        };
+
+        decoration = {
+          rounding = 4;
+          rounding_power = 4;
+        };
+
+        misc = {
+          force_default_wallpaper = 0;
+          disable_hyprland_logo = true;
+        };
+
+        cursor = {
+          inactive_timeout = 1;
         };
       };
-
-      general = {
-        gaps_in = 5;
-        gaps_out = 10;
-        border_size = 2;
-        "col.active_border" = "rgb(e66100)";
-        "col.inactive_border" = "rgba(e6610044)";
-        resize_on_border = false;
-        allow_tearing = false;
-        layout = "dwindle";
-      };
-
-      dwindle = {
-        pseudotile = true;
-        preserve_split = true;
-      };
-
-      decoration = {
-        rounding = 4;
-        rounding_power = 4;
-      };
-
-      misc = {
-        force_default_wallpaper = 0;
-        disable_hyprland_logo = true;
-      };
-
-      cursor = {
-        inactive_timeout = 1;
-      };
-    };
   };
 
   services.hyprpaper = {
