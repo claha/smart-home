8256ede2e99a577cdfcf0ef2b5b8084fba424192
Claes Hallström <hallstrom.claes@gmail.com>
2026-05-04 22:19:55 +0200

    chore(nixos): increase download buffer size

--- nixos/hosts/common.nix
+++ nixos/hosts/common.nix
@@ -14,6 +14,7 @@
         "flakes"
       ];
       warn-dirty = false;
+      download-buffer-size = "32M";
       extra-substituters = [ "https://numtide.cachix.org" ];
       extra-trusted-public-keys = [ "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g=" ];
       trusted-users = [ "@wheel" ];
