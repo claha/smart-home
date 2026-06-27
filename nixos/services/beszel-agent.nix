b0670d1995dff64ba0452d132f60fcf288caf802
Claes Hallström <hallstrom.claes@gmail.com>
2026-06-27 21:10:05 +0200

    feat(beszel): enable smartmon

--- nixos/services/beszel-agent.nix
+++ nixos/services/beszel-agent.nix
@@ -4,6 +4,7 @@
   services.beszel.agent = {
     enable = true;
     openFirewall = true;
+    smartmon.enable = true;
     environment = {
       KEY = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEG2+g6mI8KiRIcCCOxAJU8OMPSvEy0ks33cayUAKPP8";
     };
